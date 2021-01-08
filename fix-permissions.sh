d28b7c77ed2ad2e694bab942656e021c820f4278
Andrzej Piotrowski <kontakt@stronawww.net>
2021-01-08 11:53:53 +0100

    Add default values for user and group parameters

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -3,22 +3,23 @@
 # This script configures WordPress file permissions based on recommendations
 # from http://codex.wordpress.org/Hardening_WordPress#File_permissions
 #
-# Author: Michael Conigliaro <mike [at] conigliaro [dot] org>
+# Author: Andrzej Piotrowski <andrzejp [at] gmail [dot] com>
 #
 
 WP_ROOT=$1  # <-- wordpress root directory
 EDITABLE=$2 # <-- webserver group
-WP_OWNER=$3 # <-- wordpress owner
-WP_GROUP=$4 # <-- wordpress group
-
-# Check the arguments before proceeding
+WP_OWNER=${3:-www-data} # <-- wordpress owner
+WP_GROUP=${4:-www-data} # <-- wordpress group
 
 # If user, returns number. Not a user, no value
 ISUSER=$(id -u $WP_OWNER 2> /dev/null)
 # Is group in the group file? If so, returns line
 ISGRP=$(egrep -i $WP_GROUP /etc/group)
 
-echo "Start fix-permission"
+# Check the arguments before proceeding
+echo "========================"
+echo "= Start fix-permission ="
+echo "========================"
 
 if [[ ${#WP_ROOT} -eq 0 ]]
   then
@@ -32,21 +33,21 @@ then
   exit 1
 fi
 
+if [[ ${#ISUSER} -eq 0 ]]
+then
+    echo "${WP_OWNER} is not a user"
+    exit 1
+fi
+
+if [[ ${#ISGRP} -eq 0 ]]
+  then
+    echo "${WP_GROUP} is not a group"
+    exit 1
+fi
+
 if [[ ${EDITABLE} == "readonly" ]]
 then
   echo "Set readonly permissions"
-
-  if [[ ${#ISUSER} -eq 0 ]]
-  then
-      echo "${WP_OWNER} is not a user"
-      exit 1
-  fi
-
-  if [[ ${#ISGRP} -eq 0 ]]
-    then
-      echo "${WP_GROUP} is not a group"
-      exit 1
-  fi
 
   # reset to safe defaults
   echo "Reseting permissions to readonly values"
@@ -60,9 +61,7 @@ then
 
   chgrp ${WP_GROUP} ${WP_ROOT}/wp-config.php
   chmod 660 ${WP_ROOT}/wp-config.php
-fi
-
-if [[ ${EDITABLE} == "writable" ]]
+elif [[ ${EDITABLE} == "writable" ]]
 then
   echo "Set writable permissions"
 
@@ -78,7 +77,13 @@ then
 
   chgrp ${WP_GROUP} ${WP_ROOT}/wp-config.php
   chmod 660 ${WP_ROOT}/wp-config.php
+else
+  echo "2th parameter should be editable: readonly|writable"
+  echo "3th parameter should be user (default: www-data)"
+  echo "4th parameter should be group (default: www-data)"
 fi
 
-echo "Stop fix-permission"
+echo "======================="
+echo "= Stop fix-permission ="
+echo "======================="
 exit 1
